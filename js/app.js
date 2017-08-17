3bb3cd4760853c8ce4a6ff5542b3ef02e26c9b1b
a <>
2017-08-17 14:10:10 +0200

    Oplossing oef 2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
-function publishItems(title, genre, description) {
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+function publishItems(title, genre, description, articleArg) {
     var article = document.createElement("article");
     article.innerHTML =
         "<h3>" + title + "</h3>" +
@@ -6,6 +16,80 @@ function publishItems(title, genre, description) {
             "<span>" + description + "</span>";
     document.getElementById("items").appendChild(article);
 }
-publishItems("Matrix", "Thriller", "Did you know, Neo is the one?");
-publishItems("Moby Dick", "Drama", "Is this fish for real?");
+//publishItems("Matrix", "Thriller", "Did you know, Neo is the one?");
+//publishItems("Moby Dick", "Drama", "Is this fish for real?");
+var Greeter = (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+}());
+var Author = (function () {
+    function Author(name) {
+        this.name = name;
+    }
+    return Author;
+}());
+var Item = (function () {
+    function Item(title, genre, description) {
+        this.title = title;
+        this.genre = genre;
+        this.description = description;
+    }
+    return Item;
+}());
+var Movie = (function (_super) {
+    __extends(Movie, _super);
+    function Movie(title, genre, description, age, name) {
+        var _this = _super.call(this, title, genre, description) || this;
+        _this.age = age;
+        _this.name = name;
+        return _this;
+    }
+    Movie.prototype.render = function (element) {
+        publishItems(this.title, this.genre, this.description, element);
+    };
+    return Movie;
+}(Item));
+var Book = (function (_super) {
+    __extends(Book, _super);
+    function Book(title, genre, description, author) {
+        var _this = _super.call(this, title, genre, description) || this;
+        _this.name = name;
+        _this.author = author;
+        return _this;
+    }
+    Book.prototype.render = function (element) {
+        publishItems(this.title, this.genre, this.description, element);
+    };
+    return Book;
+}(Item));
+var Library = (function () {
+    function Library() {
+        this.items = [];
+        console.log("constructing");
+        this.items.push(new Movie("Inception", "Heist", "Dreaming stuff", 10, "tieners"));
+        this.items.push(new Movie("The Dark Knight", "Superhero", "Why so serious", 16, "pubers"));
+        this.items.push(new Book("Harry Potter", "Fantasy", "Wingardium Leviosa", "JK Rowling"));
+        this.items.push(new Movie("Matrix", "Thriller", "Did you know, Neo is the one?", 10, "tieners"));
+        this.items.push(new Book("Moby Dick", "Drama", "Is this fish for real?", "haai dunno"));
+        console.log("constructingstop");
+    }
+    Library.prototype.showAll = function () {
+        console.log("xxx");
+        for (var _i = 0, _a = this.items; _i < _a.length; _i++) {
+            var item = _a[_i];
+            item.render(null);
+        }
+    };
+    return Library;
+}());
+console.log("constructingstart");
+var lib = new Library();
+console.log("befroe");
+lib.showAll();
+console.log("after");
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-function publishItems(title:string,genre:string,description:string) : void {
+function publishItems(title: string, genre: string, description: string, articleArg: HTMLElement): void {
 
     let article: HTMLElement = document.createElement("article")
     article.innerHTML =
@@ -11,6 +11,112 @@ function publishItems(title:string,genre:string,description:string) : void {
 
 }
 
-publishItems("Matrix","Thriller","Did you know, Neo is the one?");
+//publishItems("Matrix", "Thriller", "Did you know, Neo is the one?");
 
-publishItems("Moby Dick","Drama","Is this fish for real?");
+//publishItems("Moby Dick", "Drama", "Is this fish for real?");
+
+
+class Greeter {
+    greeting: string;
+
+    constructor(message: string) {
+        this.greeting = message;
+    }
+
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+
+class Author {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+
+interface Rating {
+    age: number;
+    name: string;
+}
+
+abstract class Item {
+    title: string;
+    genre: string;
+    description: string;
+
+    constructor(title: string,
+                genre: string,
+                description: string) {
+        this.title = title;
+        this.genre = genre;
+        this.description = description;
+    }
+
+    abstract render(element: HTMLElement): void ;
+}
+
+class Movie extends Item implements Rating {
+    age: number;
+    name: string;
+
+    constructor(title: string
+        , genre: string
+        , description: string
+        , age: number
+        , name: string) {
+        super(title, genre, description);
+        this.age = age;
+        this.name = name;
+    }
+
+    render(element: HTMLElement): void {
+        publishItems(this.title, this.genre, this.description, element);
+    }
+}
+
+class Book extends Item {
+    name: string;
+    author: string;
+
+    constructor(title: string
+        , genre: string
+        , description: string
+        , author: string) {
+        super(title, genre, description);
+        this.name = name;
+        this.author = author;
+    }
+
+    render(element: HTMLElement): void {
+        publishItems(this.title, this.genre, this.description, element);
+    }
+}
+
+class Library {
+    items: Array<Item> = [];
+
+    constructor() {console.log("constructing");
+        this.items.push(new Movie("Inception", "Heist", "Dreaming stuff", 10, "tieners"));
+        this.items.push(new Movie("The Dark Knight", "Superhero", "Why so serious", 16, "pubers"));
+        this.items.push(new Book("Harry Potter", "Fantasy", "Wingardium Leviosa", "JK Rowling"));
+        this.items.push(new Movie("Matrix", "Thriller", "Did you know, Neo is the one?", 10, "tieners"));
+        this.items.push(new Book("Moby Dick", "Drama", "Is this fish for real?","haai dunno"));
+        console.log("constructingstop");
+    }
+
+    showAll() {console.log("xxx");
+        for(let item of this.items) {
+            item.render(null);
+        }
+    }
+}
+
+console.log("constructingstart");
+let lib:Library = new Library();
+console.log("befroe");
+lib.showAll();
+console.log("after");
